7592560bec8b93b01b5b364058c96fbaed040c6b
koleen.bp <koleen.bp@outlook.com>
2021-11-18 00:47:56 +0800

    completing create-item page

--- pages/create-items.js
+++ pages/create-items.js
@@ -86,6 +86,29 @@ export default function CreateItem() {
             updateFormInput({ ...formInput, name: e.target.value })
           }
         />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="Asset price in ETH"
+          className="mt-2 border rounded p-4"
+          onChange={(e) => ({
+            ...formInput,
+            price: e.target.value,
+          })}
+        />
+        <input type="file" name="Asset" className="my-4" onChange={onChange} />
+        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
+        <button
+          onClick={createMarket}
+          className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
+        >
+          Create digital asset
+        </button>
       </div>
     </div>
   )
